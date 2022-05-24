36ef4f75201708f127969413140cb78d1c9b40fa
iBel251 <ibbimran7@gmail.com>
2022-05-24 21:59:40 +0300

    Add components and design the calculator

---
+++ src/components/Calculator.js
@@ -0,0 +1,93 @@
+import React from 'react';
+
+const keys = [
+  {
+    name: 'AC',
+    class: 'btn gray',
+  },
+  {
+    name: '+/-',
+    class: 'btn gray',
+  },
+  {
+    name: '%',
+    class: 'btn gray',
+  },
+  {
+    name: '÷',
+    class: 'btn orange',
+  },
+  {
+    name: '7',
+    class: 'btn gray',
+  },
+  {
+    name: '8',
+    class: 'btn gray',
+  },
+  {
+    name: '9',
+    class: 'btn gray',
+  },
+  {
+    name: 'x',
+    class: 'btn orange',
+  },
+  {
+    name: '4',
+    class: 'btn gray',
+  },
+  {
+    name: '5',
+    class: 'btn gray',
+  },
+  {
+    name: '6',
+    class: 'btn gray',
+  },
+  {
+    name: '-',
+    class: 'btn orange',
+  },
+  {
+    name: '1',
+    class: 'btn gray',
+  },
+  {
+    name: '2',
+    class: 'btn gray',
+  },
+  {
+    name: '3',
+    class: 'btn gray',
+  },
+  {
+    name: '+',
+    class: 'btn orange',
+  },
+  {
+    name: '0',
+    class: 'btn gray zero',
+  },
+  {
+    name: '.',
+    class: 'btn gray',
+  },
+  {
+    name: '=',
+    class: 'btn orange',
+  },
+];
+
+const Calculator = () => (
+  <div className="container">
+    <from>
+      <input type="text" placeholder="0" />
+    </from>
+    <div className="keypad">
+      {keys.map((btn) => (<button type="button" key={btn.name} className={btn.class}>{btn.name}</button>))}
+    </div>
+  </div>
+);
+
+export default Calculator;
